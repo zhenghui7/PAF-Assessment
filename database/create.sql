ddb45e94f889ac7ccbba4429d0fe6a20fe547419
ZhengHui <lim_z_hui@hotmail.com>
2023-04-28 17:08:23 +0800

    a

--- database/create.sql
+++ database/create.sql
@@ -8,6 +8,13 @@
 -- mysql -u root -p beerdb < categories.sql
 -- mysql -u root -p beerdb < geocodes.sql
 -- mysql -u root -p beerdb < styles.sql
+
+-- source C:/opt/tfip/PAF-assessment/database/beers.sql
+-- source C:/opt/tfip/PAF-assessment/database/create.sql
+-- source C:/opt/tfip/PAF-assessment/database/breweries.sql
+-- source C:/opt/tfip/PAF-assessment/database/categories.sql
+-- source C:/opt/tfip/PAF-assessment/database/geocodes.sql
+-- source C:/opt/tfip/PAF-assessment/database/styles.sql
 create database beerdb;
 
 
